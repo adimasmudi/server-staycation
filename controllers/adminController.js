90d7d3a26a44e60db292758bd8a7b2f25beccebe
adimasmudi <adimasmudi@gmail.com>
2022-07-19 10:16:54 +0700

    add CRUD in category

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,37 @@
+const Category = require("../models/Category");
+
 module.exports = {
   view_dashboard: (req, res) => {
     res.render("admin/dashboard/view_dashboard");
   },
-  view_category: (req, res) => {
-    res.render("admin/category/view_category");
+  view_category: async (req, res) => {
+    const category = await Category.find();
+    // console.log(category);
+    res.render("admin/category/view_category", { category });
+  },
+  addCategory: async (req, res) => {
+    const { categoryName } = req.body;
+    await Category.create({ name: categoryName }, (err, data) => {
+      if (err) return err;
+      res.redirect("/admin/category");
+    });
+  },
+  editCategory: async (req, res) => {
+    const { id, categoryName } = req.body;
+    const category = await Category.findOne({ _id: id });
+    category.name = categoryName;
+    await category.save((err, data) => {
+      if (err) return err;
+      res.redirect("/admin/category");
+    });
+  },
+  deleteCategory: async (req, res) => {
+    const { id } = req.params;
+    const category = await Category.findOne({ _id: id });
+    await category.remove((err, data) => {
+      if (err) return err;
+      res.redirect("/admin/category");
+    });
   },
   view_bank: (req, res) => {
     res.render("admin/bank/view_bank");
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,9 @@ const adminController = require("../controllers/adminController");
 
 router.get("/dashboard", adminController.view_dashboard);
 router.get("/category", adminController.view_category);
+router.post("/category", adminController.addCategory);
+router.put("/category", adminController.editCategory);
+router.delete("/category/:id", adminController.deleteCategory);
 router.get("/bank", adminController.view_bank);
 router.get("/item", adminController.view_item);
 router.get("/booking", adminController.view_booking);
