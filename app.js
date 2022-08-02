40196329aead1b88a81a14f342c56e255a68de2c
adimasmudi <adimasmudi@gmail.com>
2022-08-02 11:29:28 +0700

    adding cors to make it accessible from client

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const cors = require("cors");
 
 // import mongoose
 const mongoose = require("mongoose");
@@ -39,6 +40,7 @@ app.use(
 );
 app.use(flash());
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
